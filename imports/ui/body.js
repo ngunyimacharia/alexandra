51b9822a9e2332198c5a80308377d1971dbea5f7
Ngunyi Macharia <ngunyimacharia@gmail.com>
2018-09-13 11:54:16 +0000

    First commit

---
+++ imports/ui/body.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import './body.html';
+import './headers.html';
+//Pages templates
+import '../pages/welcome.html';
+import '../pages/eits.html';
